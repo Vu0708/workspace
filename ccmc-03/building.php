772383195135e6f42829befa7fd6092ba1e28a6e
Vu0708 <C180134@ccg.ac.jp>
2019-10-29 01:40:58 +0000

    class commit

---
+++ ccmc-03/building.php
@@ -0,0 +1,32 @@
+<?php
+class Building {
+    private $name;
+    private $height;
+    
+    function __contrust($name,$height){
+        $this -> name = $name;
+        $this -> height = $height;
+    }
+    
+    function setName(string $name):void{
+          $this -> name = $name;
+      }
+      
+      function getName():string{
+          return $this -> name;
+      }
+      
+      function setHeight(int $height):void{
+          $this -> height = $height;
+      }
+      
+      function getHeight():int{
+          return $this -> height;
+      }
+      
+      function getProfile(){
+          $profile = "{$this -> name} の高さは {$this -> height}";
+          return $profile;
+      }
+}
+?>
